c505f1576f3e16171a8a3b7a7f36865fcc8301ed
Xuan Wang <xuanwn@google.com>
2024-08-21 22:50:32 +0000

    Add python getting started codelab

---
+++ codelabs/Getting_Started_with_gRPC_Python/start_here/route_guide_server.py
@@ -0,0 +1,57 @@
+# Copyright 2024 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Python implementation of the gRPC route guide server."""
+
+from concurrent import futures
+import logging
+
+import grpc
+import route_guide_resources
+
+import route_guide_pb2
+import route_guide_pb2_grpc
+
+
+def get_feature(feature_db, point):
+    """Returns Feature at given location or None."""
+    for feature in feature_db:
+        if feature.location == point:
+            return feature
+    return None
+
+
+class RouteGuideServicer(route_guide_pb2_grpc.RouteGuideServicer):
+    """Provides methods that implement functionality of route guide server."""
+
+    def __init__(self):
+        self.db = route_guide_resources.read_route_guide_database()
+
+    def GetFeature(self, request, context):
+        # Codelab Hint: implement GetFeature using get_feature() here.
+        pass
+
+
+def serve():
+    """
+    Codelab Hint: Logic for starting up a gRPC Server will be added here.
+    Steps include:
+     1. create gRPC server using grpc.server().
+     2. register RouteGuideServicer to the server.
+     3. start the server.
+    """
+    pass
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    serve()
