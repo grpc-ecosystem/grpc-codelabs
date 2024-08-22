16ca9af8d2496dbca343375c8ae92a0e62af172f
Larry Safran <lsafran@google.com>
2024-08-22 13:18:59 -0700

    Codelabs for Java Intro and Java Intro Streaming (#5)

---
+++ codelabs/Getting_Started_with_gRPC_Java/start_here/src/main/java/io/grpc/examples/routeguide/RouteGuideClient.java
@@ -0,0 +1,109 @@
+package io.grpc.examples.routeguide;
+
+import com.google.protobuf.Message;
+import io.grpc.Channel;
+import io.grpc.Grpc;
+import io.grpc.InsecureChannelCredentials;
+import io.grpc.ManagedChannel;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.examples.routeguide.RouteGuideGrpc.RouteGuideBlockingStub;
+import io.grpc.examples.routeguide.RouteGuideGrpc.RouteGuideStub; // For Phase 2
+import io.grpc.stub.StreamObserver; 				  // For Phase 2
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Sample client code that makes gRPC calls to the server.
+ */
+public class RouteGuideClient {
+  private static final Logger logger = Logger.getLogger(RouteGuideClient.class.getName());
+
+  private final RouteGuideBlockingStub blockingStub;
+
+  /** Construct client stub for accessing RouteGuide server using the existing channel. */
+  public RouteGuideClient(Channel channel) {
+    /****************************************************************
+     * Codelab Hint: Create a blocking stub for your service
+     * using the code generated from the proto file (RouteGuideGrpc)
+     *
+     blockingStub =
+     ****************************************************************/
+  }
+
+  /**
+   * Blocking unary call example.  Calls getFeature and prints the response.
+   */
+  public void getFeature(int lat, int lon) {
+    info("*** GetFeature: lat={0} lon={1}", lat, lon);
+
+    Point request = Point.newBuilder().setLatitude(lat).setLongitude(lon).build();
+
+    Feature feature;
+    try {
+      /****************************************************************
+       * Codelab Hint: Use the blocking stub to make an RPC call to getFeature
+       *
+       ****************************************************************/
+
+    } catch (StatusRuntimeException e) {
+      warning("RPC failed: {0}", e.getStatus());
+      return;
+    }
+    if (RouteGuideUtil.exists(feature)) {
+      info("Found feature called \"{0}\" at {1}, {2}",
+          feature.getName(),
+          RouteGuideUtil.getLatitude(feature.getLocation()),
+          RouteGuideUtil.getLongitude(feature.getLocation()));
+    } else {
+      info("Found no feature at {0}, {1}",
+          RouteGuideUtil.getLatitude(feature.getLocation()),
+          RouteGuideUtil.getLongitude(feature.getLocation()));
+    }
+  }
+
+  /** Issues several different requests and then exits. */
+  public static void main(String[] args)  {
+    String target = "localhost:8980";
+    if (args.length > 0) {
+      if ("--help".equals(args[0])) {
+        System.err.println("Usage: [target]");
+        System.err.println("");
+        System.err.println("  target  The server to connect to. Defaults to " + target);
+        System.exit(1);
+      }
+      target = args[0];
+    }
+
+        /***************************************************************
+         * Codelab Hint: create a channel using target defined above
+         *
+         ManagedChannel channel =
+         ***************************************************************/
+
+    try {
+      // Create a client instance
+      RouteGuideClient client = new RouteGuideClient(channel);
+
+      // Looking for a valid feature
+      client.getFeature(409146138, -746188906);
+
+      // Feature missing.
+      client.getFeature(0, 0);
+    } finally {
+      channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+    }
+  }
+
+  private void info(String msg, Object... params) {
+    logger.log(Level.INFO, msg, params);
+  }
+
+  private void warning(String msg, Object... params) {
+    logger.log(Level.WARNING, msg, params);
+  }
+
+}
