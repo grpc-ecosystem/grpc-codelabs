325454b56f75a2c29f4d0fceaeab2b93a55f9247
Nathaniel Ford <nathaniel.ford@gmail.com>
2026-09-01 17:32:28 +0000

    Fix generated code, proto ordering.

--- codelabs/grpc-rust-getting-started/start_here/build.rs
+++ codelabs/grpc-rust-getting-started/start_here/build.rs
@@ -1,21 +1,20 @@
 use std::env;
-use std::path::PathBuf;
+use std::path::Path;
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-env-changed=GRPC_RUST_REGENERATE_PROTO");
 
     if env::var_os("GRPC_RUST_REGENERATE_PROTO").is_some() {
-        let manifest_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
-        let generated_dir = manifest_dir.join("generated");
+        let generated_dir = Path::new("generated");
         if generated_dir.exists() {
-            std::fs::remove_dir_all(&generated_dir)
+            std::fs::remove_dir_all(generated_dir)
                 .expect("All files in generated/ directory should be deletable");
         }
 
         grpc_protobuf_build::CodeGen::new()
             .output_dir(generated_dir)
-            .include(manifest_dir.join("proto"))
+            .include("proto")
             .input("routeguide.proto")
             .compile()
             .unwrap();
